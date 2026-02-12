3bf29ff450c94a26a8a6b74e98e472ff468a8395
Yesid Londoño <yesidlondono342@gmail.com>
2026-02-12 17:43:03 -0500

    Docs: implementar documentación swagger

--- src/main/java/com/enterprise/posapp/productos/controller/MenuController.java
+++ src/main/java/com/enterprise/posapp/productos/controller/MenuController.java
@@ -2,6 +2,9 @@ package com.enterprise.posapp.productos.controller;
 
 import com.enterprise.posapp.productos.dto.response.ProductoResponse;
 import com.enterprise.posapp.productos.service.ProductoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,6 +21,10 @@ public class MenuController {
     private final ProductoService productoService;
 
     @GetMapping
+    @Operation(summary = "Mostrar el menú", description = "Retorna todos los productos")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "muestra los productos")
+    })
     public Page<ProductoResponse> menu(Pageable pageable) {
         return productoService.obtenerTodosLosProductos(pageable);
     }

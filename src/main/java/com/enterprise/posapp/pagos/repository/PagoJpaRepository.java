db3d577cb3f3dd81b30d1135e0c8bc811ff9601e
Yesid Londoño <yesidlondono342@gmail.com>
2026-03-08 21:46:12 -0500

    refactor: ajustar autenticación, repositorio de pagos y seed de roles órdenes

--- src/main/java/com/enterprise/posapp/pagos/repository/PagoJpaRepository.java
+++ src/main/java/com/enterprise/posapp/pagos/repository/PagoJpaRepository.java
@@ -3,7 +3,9 @@ package com.enterprise.posapp.pagos.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.enterprise.posapp.pagos.model.entity.Pagos;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PagoJpaRepository extends JpaRepository<Pagos, Long> {
 
 }
